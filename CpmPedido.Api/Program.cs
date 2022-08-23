ddae0cc3987425b864e1d651eca237b8d0bd6590
Pedro Batista <pedrohenrique.sb07@gmail.com>
2022-08-23 20:39:05 -0300

    Configuration DbContext

--- CpmPedido.Api/Program.cs
+++ CpmPedido.Api/Program.cs
@@ -1,8 +1,21 @@
 using CpmPedido.Api;
+using CpmPedido.Repository.Common;
+using Microsoft.EntityFrameworkCore;
+using Npgsql;
+using System.Data.Common;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+var dbConnection = new NpgsqlConnection(builder.Configuration.GetConnectionString("ConnectionString"));
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseNpgsql(
+        dbConnection,
+        assembly => assembly.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName));
+});
 
 DependencyInjector.Register(builder.Services);
 
---
+++ CpmPedido.Repository/Common/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CpmPedido.Repository.Common
+{
+    public class ApplicationDbContext : DbContext
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+        }
+
+        public ApplicationDbContext()
+        {
+            ChangeTracker.AutoDetectChangesEnabled = false;
+        }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            ChangeTracker.AutoDetectChangesEnabled = false;
+        }
+    }
+}
